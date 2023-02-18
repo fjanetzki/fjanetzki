54ce11b336aa07f3bc2e523bff0edb04fb0f6d9c
Fabian Janetzki <fabian.janetzki@web.de>
2023-02-18 19:23:35 +0100

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import linkedinLogo from './svg/linkedin-icon-2-logo-svgrepo-com.svg'
+import xingLogo from './svg/xing-icon-logo-svgrepo-com.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Logo-Container">
+        <img src={linkedinLogo} alt="LinkedIn logo" />
+        <img src={xingLogo} alt="Xing logo" />
+      </div>
     </div>
   );
 }
